acf7d253463caa614e8ab817b56f7b9b3210a3aa
lucazweb <lucazwebmail@gmail.com>
2020-04-12 19:29:54 -0300

    Fixes eslint bad config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import store from './store';
-import { Provider  } from 'react-redux';
-import Wrapper from './Wrapper';
+import Routes from './Routes';
+import { Provider } from 'react-redux';
 
 const App = () => (
   <Provider store={store}>
-    <Wrapper />
+    <Routes />
   </Provider>
 );
 
